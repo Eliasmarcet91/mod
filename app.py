7ada94ab841e55e2938c94ebe0b60f0229e47284
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-01-19 21:33:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from openai import OpenAI
 import json
-from PIL import Image
 
 # Function to serialize the output
 def serialize(obj):
@@ -45,16 +44,17 @@ if st.button('Moderate'):
 
 
 #add tabs to the UI
-def pages():
-    page1, page2, page3 = st.tabs(['Cloud Adoption', 'CyberSecurity', 'Linux'])
-    with page1:
-        st.write("Steps for a successful Cloud Adoption.")
-    with page2:
-        st.write("What is CyberSecurity?")
-    with page3:
-        st.write("Introduction to Linux.")
-        image = Image.open('https://i.etsystatic.com/21812990/r/il/1b6940/2314861823/il_fullxfull.2314861823_gvzf.jpg')
-        st.image(image, caption='Its all about the penguin')
+
+page1, page2, page3 = st.tabs(['Cloud Adoption', 'CyberSecurity', 'Linux'])
+with page1:
+    st.write("Steps for a successful Cloud Adoption.")
+with page2:
+    st.write("What is CyberSecurity?")
+with page3:
+    st.write("Introduction to Linux.")
+    from PIL import Image
+    image = Image.open('https://i.etsystatic.com/21812990/r/il/1b6940/2314861823/il_fullxfull.2314861823_gvzf.jpg')
+    st.image(image, caption='Its all about the penguin')
     
 #image = Image.open('https://i.etsystatic.com/21812990/r/il/1b6940/2314861823/il_fullxfull.2314861823_gvzf.jpg')
 #st.image(image, caption='All bald men have a big forehead')
